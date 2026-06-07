2b7032bec02ed1b9eb2a2874cbadb516b6d81cca
pedroPPV <peterpaivav@gmail.com>
2026-06-07 13:55:07 -0300

    Fase um implementando com suas instancias aleatorias

--- src/Fase.cpp
+++ src/Fase.cpp
@@ -14,11 +14,29 @@ Fase::~Fase() {
 
 void Fase::criarInmFaceis(Jogador* j) {
 	Inimigo::sementear();
-	int MAX = rand() % 2 + 3;
+	int MAX = rand() % 3 + 5;
 
 	for (int i = 0; i < MAX;i++) {
 		Drone* d1 = new Drone(NULL, "Texturas/Drone/drone_somente.png");
-		d1->setPos(100.0f + rand()%500 * i, 200.0f * i);
+		sf::Vector2f p(0.0f, 0.0f);
+		if (i < 2) {
+			p.x = 500.0f + 1000.0f * i;
+			p.y = 850.0f;
+		}
+		else if (i >= 2 && i < 5) {
+			p.x = 200.0f + 700.0f * (i - 2);
+			p.y = 710.0f;
+		}
+
+		else if (i >= 5 && i < 7) {
+			p.x = 400.0f + 1000.0f * (i - 5);
+			p.y = 580.0f;
+		}
+		else if (i >= 7) {
+			p.x = 200.0f + 700.0f * (i - 7);
+			p.y = 480.0f;
+		}
+		d1->setPos(p.x, p.y);
 		d1->setJog(j);
 		GC.incluirInimigo(d1);
 		list_ents.incluir(d1);
@@ -27,11 +45,33 @@ void Fase::criarInmFaceis(Jogador* j) {
 
 void Fase::criarPlataformas() {
 	Inimigo::sementear();
-	int MAX = 2;
+	int MAX = rand()%3 + 9;
 
 	for (int i = 0;i < MAX;i++) {
-		Plataforma* p = new Plataforma(sf::Vector2f(400.0f - 100 * i, 900.0f - 150 * i), sf::Vector2f(300.0f, 42.0f));
-		GC.incluirObstaculo(p);
-		list_ents.incluir(p);
+		sf::Vector2f p(0.0f, 0.0f);
+		if (i < 2) {
+			p.x = 400.0f + 1000.0f * i;
+			p.y = 900.0f;
+		}
+		else if (i>=2 && i<5){
+			p.x = 200.0f + 700.0f*(i-2);
+			p.y = 770.0f;
+		}
+
+		else if (i >= 5 && i<7) {
+			p.x = 400.0f + 1000.0f * (i-5);
+			p.y = 640.0f;
+		}
+		else if (i>= 7 && i<10) {
+			p.x = 200.0f + 700.0f * (i - 7);
+			p.y = 510.0f;
+		}
+		else if (i >=10) {
+			p.x = 400.0f + 1000.0f * (i - 10);
+			p.y = 380.0f;
+		}
+		Plataforma* p1 = new Plataforma(sf::Vector2f(p.x,p.y), sf::Vector2f(300.0f, 42.0f));
+		GC.incluirObstaculo(p1);
+		list_ents.incluir(p1);
 	}
 }
--- src/FaseUm.cpp
+++ src/FaseUm.cpp
@@ -22,17 +22,21 @@ void FaseUm::criarIniMed() {
 
 void FaseUm::criarObsMed() {
 	Inimigo::sementear();
-	int MAX = /*rand() % 2 + */ 3;
+	int MAX = 5;//rand() % 3 + 3;
 
 	for (int i = 0;i < MAX;i++) {
 		sf::Vector2f p(0.0f, 0.0f);
-		if (i < 2) {
-			p.x = 400.0f + 400.0f * i;
+		if (i < 1) {
+			p.x = 950.0f;
 			p.y = 1012.0f;
 		}
-		else {
-			p.x = 600.0f;
+		else if(i>=1 && i<3){
+			p.x = 500.0f + 1000*(i-1);
 			p.y = 883.0f;
+		}
+		else if (i >= 3) {
+			p.x = 300.0f + 700 * (i - 3);
+			p.y = 753.0f;
 		}
 
 			Arbusto* a = new Arbusto(sf::Vector2f(p.x,p.y), sf::Vector2f(29.0f, 17.0f));
